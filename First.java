9bd313d438fb16724e59d6039a5fc8f5dc3f4220
milanranpara <milanranpara1@gmail.com>
2025-06-12 16:42:31 +0530

    Adding loop!

--- First.java
+++ First.java
@@ -3,5 +3,8 @@ class First{
         System.out.println("\nHello,");
         System.out.println("This is the First.java program file.");
         System.out.println("Marvel Avengers! \n");
+        for(int i=0; i<6; i++){
+            System.out.println(i);
+        }
     }
 }
